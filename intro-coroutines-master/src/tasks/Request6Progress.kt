0a36076b07bad1669f30d5fff20c12084d10ea42
JJJoonngg <whdtls3878@gmail.com>
2022-04-10 23:02:48 +0900

    Showing progress

--- intro-coroutines-master/src/tasks/Request6Progress.kt
+++ intro-coroutines-master/src/tasks/Request6Progress.kt
@@ -7,5 +7,21 @@ suspend fun loadContributorsProgress(
     req: RequestData,
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .body() ?: listOf()
+
+    val allUsers = mutableListOf<User>()
+
+    for ((index, repo) in repos.withIndex()) {
+        val users = service
+            .getRepoContributors(req.org, repo.name)
+            .also { logUsers(repo, it) }
+            .bodyList()
+        allUsers += users
+        allUsers.aggregate()
+        val isCompleted = index == repos.lastIndex
+        updateResults(allUsers, isCompleted)
+    }
 }
