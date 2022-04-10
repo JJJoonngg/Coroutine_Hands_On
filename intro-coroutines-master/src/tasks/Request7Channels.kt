c017cea624d2deeb61546eaf66693f94e3f1174f
JJJoonngg <whdtls3878@gmail.com>
2022-04-10 23:08:16 +0900

    Channels

--- intro-coroutines-master/src/tasks/Request7Channels.kt
+++ intro-coroutines-master/src/tasks/Request7Channels.kt
@@ -1,6 +1,8 @@
 package tasks
 
 import contributors.*
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
@@ -11,6 +13,30 @@ suspend fun loadContributorsChannels(
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
     coroutineScope {
-        TODO()
+
+        val repos = service
+            .getOrgRepos(req.org)
+            .also { logRepos(req, it) }
+            .body() ?: listOf()
+
+        val allUsers = mutableListOf<User>()
+        val channel = Channel<List<User>>()
+
+        fojjjoonnggr (repo in repos) {
+            launch {
+                val users = service
+                    .getRepoContributors(req.org, repo.name)
+                    .also { logUsers(repo, it) }
+                    .bodyList()
+                channel.send(users)
+            }
+        }
+
+        repeat(repos.size) { index ->
+            val users = channel.receive()
+            allUsers += users
+            val isCompleted = index == repos.lastIndex
+            updateResults(allUsers.aggregate(), isCompleted)
+        }
     }
 }
