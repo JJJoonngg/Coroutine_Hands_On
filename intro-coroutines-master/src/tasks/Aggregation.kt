baa2c9b1958335f974463eecc2e35d7e70ba6b86
JJJoonngg <whdtls3878@gmail.com>
2022-04-10 22:42:29 +0900

    aggregate() modify

--- intro-coroutines-master/src/tasks/Aggregation.kt
+++ intro-coroutines-master/src/tasks/Aggregation.kt
@@ -14,5 +14,10 @@ TODO: Write aggregation code.
  The corresponding test can be found in test/tasks/AggregationKtTest.kt.
  You can use 'Navigate | Test' menu action (note the shortcut) to navigate to the test.
 */
-fun List<User>.aggregate(): List<User> =
-    this
+fun List<User>.aggregate(): List<User> {
+    return groupBy { it.login }
+        .map { (login, users) ->
+            User(login, users.sumOf { it.contributions })
+        }
+        .sortedByDescending { it.contributions }
+}
